31be0ed09ae2182302eac3e7c08923714057af3c
Cameron McCord <cameronmccord2@gmail.com>
2015-04-14 08:52:26 -0600

    finished chapter 4

--- sample_app/config/initializers/backtrace_silencers.rb
+++ sample_app/config/initializers/backtrace_silencers.rb
@@ -5,3 +5,5 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+# This silences the errors we dont care about
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
