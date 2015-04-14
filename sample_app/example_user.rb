cd7187dd103e160be81d4f320236c7c032bb42d3
Cameron McCord <cameronmccord2@gmail.com>
2015-04-14 14:22:59 -0600

    finished chapter 6

---
+++ sample_app/example_user.rb
@@ -0,0 +1,12 @@
+class User
+	attr_accessor :name, :email # creates getters and setters
+
+	def initialize(attributes = {})
+		@name = attributes[:name]
+		@email = attributes[:email]
+	end
+
+	def formatted_email
+		"#{@name} <#{@email}>"
+	end
+end
