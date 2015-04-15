20185eb303962de91813824c2d8741a28da12ad9
Cameron McCord <cameronmccord2@gmail.com>
2015-04-15 08:51:13 -0600

    added ssl for production using puma

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -16,6 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	assert_difference "User.count", 1 do 
   		post_via_redirect users_path, user: {name: "Example User", email:"user@example.com", password:"password", password_confirmation:"password"}
   	end
-  	assert_template 'users/show'
+  	assert_template 'users/showGem'
   end
 end
