86ad0d59bbc34073111bce92864260da3d2f831a
Cameron McCord <cameronmccord2@gmail.com>
2015-04-15 09:26:14 -0600

    chapter 7 exersizes done

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -11,11 +11,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "signup accepts a valid submission" do 
-  	get signup_path
-  	assert_response :success
-  	assert_difference "User.count", 1 do 
-  		post_via_redirect users_path, user: {name: "Example User", email:"user@example.com", password:"password", password_confirmation:"password"}
-  	end
-  	assert_template 'users/showGem'
+    get signup_path
+    assert_response :success
+    assert_difference "User.count", 1 do 
+      post_via_redirect users_path, user: {name: "Example User", email:"user@example.com", password:"password", password_confirmation:"password"}
+    end
+    assert_template 'users/show'
+
+    # test for the flash to be there
+    assert_select "div.alert-success", 1
+    assert flash.any?
+  end
+
+  test "signup denies an invalid submission" do 
+    get signup_path
+    assert_response :success
+    assert_no_difference "User.count" do 
+      post_via_redirect users_path, user: {name: "Example User", email:"user@example", password:"pass", password_confirmation:"pass"}
+    end
+    assert_template 'users/new' #the submission fails and it returns to the submission page
+
+    assert_select "div.alert-danger", "The form contains 2 errors"
+    assert_select "ul#signup_errors", 1
   end
 end
