6e2c5debd453705dbe8029ede8ffe91af0770a20
Cameron McCord <cameronmccord2@gmail.com>
2015-04-17 10:45:06 -0600

    account activation complete with tests

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,11 +5,15 @@ class SessionsController < ApplicationController
   def create
   	@user = User.find_by(email: params[:session][:email].downcase)
   	if @user && @user.authenticate(params[:session][:password])
-  		# Log the user in, taken to the user's show page
-  		log_in @user
-  		params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-  		@user.remember #This was different than the tutorial to get it to pass, before we were calling 'remember user' which was causing a cookies save again
-  		redirect_back_or @user
+  		if @user.activated?
+  			log_in @user
+  			params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+	  		@user.remember #This was different than the tutorial to get it to pass, before we were calling 'remember user' which was causing a cookies save again
+	  		redirect_back_or @user
+	  	else
+	  		flash[:warning] = "Account not activated. Check your email for the activation link."
+	  		redirect_to root_url
+	  	end
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
